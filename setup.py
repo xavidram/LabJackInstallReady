b52a4dfac98e7dbce8e87da76bfd4f4a3adbe2bc
Xavid Ramirez <xavid.ramirez01@utrgv.edu>
2016-06-13 11:03:33 -0500

    making changes

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os, subprocess
 #install essentials and base tools
 subprocess.call(["sudo" , "apt-get" , "-y" , "update"])
 subprocess.call(["sudo" , "apt-get" , "-y" , "upgrade"])
-subprocess.call(["sudo" , "apt-get" , "install" , "libusb-1.0" , "git"])
+subprocess.call(["sudo" , "apt-get" , "install" ,"-y", "libusb-1.0" , "git"])
 
 #pull git repositories
 #exo driver first
 subprocess.call(["git" , "clone" , "https://github.com/labjack/exodriver.git"])
-subprocess.call(["python" , "exodriver/setup.py"])
+subprocess.call("./install.sh" , cwd="/home/pi/LabJackInstallReady/exodriver/")
 #labjackpython library
 subprocess.call(["git" , "clone" , "https://github.com/labjack/LabJackPython.git"])
-subprocess.call(["python" , "LabJackPython/setup.py"])
+subprocess.call(["python" , "setup.py" , "install"] , cwd="/home/pi/LabJackInstallReady/LabJackPython/")
 
 #setup complete.
